d2b85482f57f8a07b6f85e4ace9ff75e50421833
Frederik Ar. Mikkelsen <fred@frederikam.com>
2020-05-18 23:06:07 +0200

    Cleanup

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,73 @@
 
 
-import starter.gameLoop
+import memory.role
+import memory.sources
+import roles.IRole
+import roles.roles
+import screeps.api.Creep
+import screeps.api.FIND_MY_CREEPS
+import screeps.api.Game
+import screeps.api.Memory
+import screeps.api.Record
+import screeps.api.Room
+import screeps.api.component1
+import screeps.api.component2
+import screeps.api.get
+import screeps.api.iterator
+import screeps.api.structures.StructureSpawn
+import screeps.api.values
+import screeps.utils.contains
+import screeps.utils.isEmpty
+import screeps.utils.unsafe.delete
+import strucures.runSpawnLogic
 
 /**
  * Called by Screeps
  */
 @Suppress("unused")
 fun loop() {
-    gameLoop()
+    val mainSpawn: StructureSpawn = Game.spawns.values.firstOrNull() ?: return
+
+    //delete memories of creeps that have passed away
+    houseKeeping(Game.creeps)
+
+    for ((_, creep) in Game.creeps) {
+        val role = roles[creep.memory.role] as IRole?
+        try {
+            role?.loop(creep)
+        } catch (e: Throwable) {
+            println("${creep.name}: $e")
+        }
+    }
+
+    for ((_, room) in Game.rooms) {
+        try {
+            runSpawnLogic(room)
+        } catch (e: Throwable) {
+            println("Failed doing spawn logic in $room")
+            console.log(e)
+        }
+    }
+}
+
+val Room.myCreeps get() = find(FIND_MY_CREEPS)
+
+private fun houseKeeping(creeps: Record<String, Creep>) {
+    if (Game.creeps.isEmpty()) return  // this is needed because Memory.creeps is undefined
+
+    for ((creepName, _) in Memory.creeps) {
+        if (creeps[creepName] == null) {
+            console.log("deleting obsolete memory entry for creep $creepName")
+            delete(Memory.creeps[creepName])
+        }
+    }
+
+    for((_, room) in Game.rooms) {
+        val sources = room.memory.sources ?: continue
+        sources.forEach {
+            if (!Game.creeps.contains(it.harvester ?: "")) {
+                it.harvester = null
+            }
+        }
+    }
 }
--- src/main/kotlin/memory/creep.kt
+++ src/main/kotlin/memory/creep.kt
@@ -1,6 +1,10 @@
 package memory
 
+import screeps.api.Creep
 import screeps.api.CreepMemory
+import screeps.api.Game
+import screeps.api.Room
+import screeps.api.get
 import screeps.utils.memory.memory
 
 val CreepMemory.isGathering get() = state == "gathering"
@@ -10,6 +14,7 @@ fun CreepMemory.setDepositing() { state = "depositing"}
 
 var CreepMemory.role by memory { "none" }
 var CreepMemory.room by memory<String> { throw notInitialised() }
+val Creep.homeRoom get(): Room? = Game.rooms[memory.room]
 var CreepMemory.target by memory<String>()
 var CreepMemory.state by memory<String>()
 var CreepMemory.source by memory<String>()
